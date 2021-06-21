33c99a77fe6d8753f0bd771cae30f76330a0a2c8
Donald Schrimsher <don.schrimsher@testdouble.com>
2021-06-21 13:55:42 -0400

    Set ZSH as the default shell

--- install.sh
+++ install.sh
@@ -2,3 +2,5 @@
 git config --global core.editor vim
 # Install Oh My ZSH
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# Set ZSH as the default shell
+chsh -s /usr/bin/zsh
